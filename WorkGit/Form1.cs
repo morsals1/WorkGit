90736715eb68e55c7d232cdfb266ee854832e473
morsals1 <157448960+morsals1@users.noreply.github.com>
2025-03-13 11:01:47 +0500

    Add files via upload

--- WorkGit/Form1.cs
+++ WorkGit/Form1.cs
@@ -19,6 +19,8 @@ namespace WorkGit
 
         private void button1_Click(object sender, EventArgs e)
         {
+            Random rnd = new Random();
+            label1.Text = $" {rnd.Next()}";
         }
     }
 }
